ff28c548f4be388952f58fa9ee3b33fcc1cf4b36
ADONIS NOA FROMETA <anoaf@ucenfotec.ac.cr>
2024-12-22 22:02:46 -0600

    Se añade de fichero 01_variable.py

---
+++ 01_variables.py
@@ -0,0 +1,26 @@
+my_string_variable="My String Variable"
+print (my_string_variable)
+
+my_int_variable = 5;
+print (my_int_variable);
+
+my_bool_variable = False;
+print (my_bool_variable);
+
+# Concatenación de variables
+print(my_string_variable,",", my_int_variable,",", my_bool_variable);
+
+# Funcion de len (permite contar la caitidad de caracteres más los espacios )
+print (len(my_string_variable));
+
+# Declaración de variavles en una línea
+name, surname, alias, age = "Adonis","Noa","Noafro", 34;
+print ("Mi nombre completo es:",name,surname,". Mi alias es:", alias, "y tengo ", age,"año de edad." );
+
+#input o entrada de datos
+name= (input("Digite su nombre"));
+age= (input("Digite su edad"));
+
+print(name);
+print(age);
+

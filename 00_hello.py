08fb9deb145d8030148abc0f87b639cf54a0c509
ADONIS NOA FROMETA <anoaf@ucenfotec.ac.cr>
2024-12-22 22:00:20 -0600

    Se cabia el nombre de fichero hello.py a 00_hello.py

---
+++ 00_hello.py
@@ -0,0 +1,36 @@
+# Esto es un comentario
+# Nuestro Hola Mundo en Python
+print ("Hola Python")
+print ('Hola Python')
+
+
+
+"""
+Este es un 
+comentario
+en varias líneas
+"""
+
+'''
+Este tambie  es un 
+comentario 
+en varias lineas 
+'''
+
+#Operaciones matemáticas
+print ("La suma de 6 y 6 es: ", (6 + 6))  # Suma (+)
+print ("La resta de 6 y 5 es: ", (6 - 5)) # Resta(-)
+print ("La división de 10 y 5 es: ", (10 / 5)) # división(/)
+print ("La multiplicación de 6 y 5 es: ", (6 * 5)) # Multiplicación(*)
+print ("La potencia de 6 elevado a 2 es: ", (6 ** 2)) # Potencia o Exponencial(**)
+print ("El módulo de  6 y 3 es: ", (6 % 3)) # Módulo(%)
+print ((25 //2)) # Floor division operator
+
+
+# Consultar el tipo de dato 
+print (type ("Hola Python")) # Tipo 'str'
+print (type (5)) # Tipo 'int'
+print (type (1.5)) # Tipo 'float'
+print (type (3+1j)) # Tipo 'complex'
+print (type ([1, 2, 3])) # Tipo 'list'
+print (type (True)) # Tipo 'list'

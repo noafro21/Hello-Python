169abea5e20ccd89d7fa560b5d5564ab1f03038e
ADONIS NOA FROMETA <anoaf@ucenfotec.ac.cr>
2024-12-23 21:40:50 -0600

    Se agrega fichero Con operadores aritméticas, operadores para comparar y operadores lógicos

---
+++ 02_operadores.py
@@ -0,0 +1,40 @@
+#Operadores Aritméticos
+print ("La suma de 6 y 6 es: ", (6 + 6))  # Suma (+)
+print ("La resta de 6 y 5 es: ", (6 - 5)) # Resta(-)
+print ("La división de 10 y 5 es: ", (10 / 5)) # división(/)
+print ("La multiplicación de 6 y 5 es: ", (6 * 5)) # Multiplicación(*)
+print ("La potencia de 6 elevado a 2 es: ", (6 ** 2)) # Potencia o Exponencial(**)
+print ("El módulo de  6 y 3 es: ", (6 % 3)) # Módulo(%)
+print ((25 // 2)) # Floor division operator
+
+print("Hola " + "Python " + "¿Qué tal? ")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2**3))
+
+my_float = 2.5 *5
+print("Hola " * int(my_float))
+
+### Operadores para comparar ###
+print(3 > 4)   # False
+print(3 < 4)   # True
+print(3 >= 4)  # Flase
+print(3 <= 4)  # True
+print(3 == 4)  # False
+print(3 != 4)  # True
+print()
+print("Hola" > "Python") # False
+print("Hola" < "Python") # True
+print("aaa" >= "abaa")   # False
+print(len("aaaa") >= len("abaa")) # True (Cuenta los caracteres)
+print("Hola" <= "Python") # True
+print('Hola'=='Hola')     # True
+print('Hola' != 'Python') # True
+print()
+
+### Operadores lógicos ###
+print(3 > 4 and "Hola" > "Python") # False
+print(3 < 4 or  "Hola" > "Python") # True
+#print(3 > 4 not "Hola" > "Python")
+
+
